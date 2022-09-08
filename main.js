3ce2277487c9a7b7dc7f84712a1f91918e0165ff
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2022-09-09 00:03:00 +0300

    almost all are ready , except bug with

--- main.js
+++ main.js
@@ -1,46 +1,76 @@
-    const inputMessage = document.querySelector(".message"),
+    const inputMessage = document.querySelector(".create_new_todo__message"),
           addButton = document.querySelector(".add"),
-          toDoBlock = document.querySelector(".todo")
+          toDoBlock = document.querySelector(".todo"),
+          warningBlock = document.querySelector(".todo_list__warning")
 
-    let arr = []
-    let tasks = localStorage.getItem("tasks")
+    let tasks
+
+    !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem("tasks"))
 
     function CreateTask (text) {
         this.text = text
+        this.completed = false
     }
 
-    const createTemplateList = (text , id) => {
+    const createTemplateList = (task , id) => {
         return `
-            <li class="todo__list">
-                <input type="checkbox" id="todo__input-${id}">
-                <label for="todo__input-${id}">${text}</label>
-                <img src="./image/trash.svg" alt="trash">
+            <li class="todo__list ${task.completed ? 'opacity' : ''}" >
+                <input onclick="checkedTask(${id})" class="todo__input" 
+                        type="checkbox" id="todo__input-${id}" ${task.completed ? 'checked' : ''}>
+                <label class="todo__label" for="todo__input-${id}">${task.text}</label>
+                <img onclick="deletePoint(${id})" src="./image/trash.svg" class="trash" alt="trash">
             </li>
         `
     }
 
-    const workItemsStorage = () => {
-        toDoBlock.innerHTML = ""
-        JSON.parse(localStorage.getItem("tasks")).forEach((item , id)  => {
-            toDoBlock.innerHTML += createTemplateList(item.text , id)
-        })
+    const checkedTask = (index) => {
+        tasks[index].completed = !tasks[index].completed
+        if(tasks[index].completed === true){
+            document.querySelectorAll(".todo__list")[index].classList.add("opacity")
+        }else{
+            document.querySelectorAll(".todo__list")[index].classList.remove("opacity")
+        }
+        localStorage.setItem("tasks" , JSON.stringify(tasks))
+        // fillHtmlList()
     }
 
-    tasks ? workItemsStorage() : alert("В хранилище тасков нет , заполните , пожалуйста! ")
+    const filterTasks = () => {
+        const activeTasks = tasks.length && tasks.filter(item => item.completed === false)
+        const completedTasks = tasks.length && tasks.filter(item => item.completed === true)
+        tasks = [...activeTasks , ...completedTasks]
+    }
+
+    const fillHtmlList = () => {
+        toDoBlock.innerHTML = ""
+        if(tasks.length > 0){
+            filterTasks()
+            tasks.forEach((item , id)  => {
+                toDoBlock.innerHTML += createTemplateList(item , id)
+            })
+        }
+    }
+
+    fillHtmlList()
 
     addButton.addEventListener("click" , () => {
-        arr.push(new CreateTask(inputMessage.value))
-        inputMessage.value = ""
-        localStorage.setItem("tasks" , JSON.stringify(arr))
-        workItemsStorage()
+
+            if(inputMessage.value !== "") {
+                warningBlock.textContent = ""
+                tasks.push(new CreateTask(inputMessage.value))
+                inputMessage.value = ""
+                localStorage.setItem("tasks", JSON.stringify(tasks))
+                fillHtmlList()
+            }else{
+                warningBlock.textContent = "У вас не заполненное поле"
+                warningBlock.style.color = "#ffd86f"
+                warningBlock.style.textAlign = "center"
+            }
+
     })
 
-    try{
-        let trashPoint = document.querySelector("[alt = 'trash']")
-        trashPoint.addEventListener("click" , () => {
-            console.log("Кликаем по мусорке")
-        })
-    }catch(e){
-        console.log(e.message)
+    const deletePoint = (index) => {
+        tasks.splice(index , 1)
+        localStorage.setItem("tasks" , JSON.stringify(tasks))
+        fillHtmlList()
     }
 
